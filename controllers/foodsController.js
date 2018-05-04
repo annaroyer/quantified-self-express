659606459d51dc57e46484f09387e0aacb667ae7
Anna Royer <anna.royer@colorado.edu>
2018-05-04 16:02:03 -0600

    account for id that does not exist in foods controller show and add foods controller create

--- controllers/foodsController.js
+++ controllers/foodsController.js
@@ -1,6 +1,7 @@
 const Food = require('../models/food')
 
 class FoodsController {
+
   static index(request, response, next){
     Food.all()
     .then(foods => response.status(200).json(foods))
@@ -9,12 +10,17 @@ class FoodsController {
   static show(request, response, next){
     Food.find(request.params.id)
     .then(food => {
-      if (food){
+      if(food){
         response.json(food)
       } else {
         return response.sendStatus(404)
       }
     })
+  }
+
+  static create(request, response, next){
+    Food.create(request.body.food)
+    .then(food => response.status(201).json(food))
   }
 }
 
