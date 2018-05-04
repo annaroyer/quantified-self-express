fc781a3e1329d3b8cb6a2f08dbef0b15275c8e52
Anna Royer <anna.royer@colorado.edu>
2018-05-04 00:21:25 -0600

    refactor into mvc

--- controllers/foodsController.js
+++ controllers/foodsController.js
@@ -1,8 +1,14 @@
 const Food = require('../models/food')
 
 class FoodsController {
-  static index(){
-    return Food.all()
+  static index(request, response, next){
+    Food.all()
+    .then(function(foods){
+      response.status(200).json(foods)
+    })
+    .catch(function(error){
+      response.status(500).json({ error })
+    })
   }
 }
 
