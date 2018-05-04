6e1fe982391b7d4e9d278b8d4bc38d1acf379efa
Anna Royer <anna.royer@colorado.edu>
2018-05-04 09:22:39 -0600

    add if else to food controller show to return 404 if food is not found

--- controllers/foodsController.js
+++ controllers/foodsController.js
@@ -4,13 +4,17 @@ class FoodsController {
   static index(request, response, next){
     Food.all()
     .then(foods => response.status(200).json(foods))
-    .catch(error => response.status(500).json({ error }))
   }
 
   static show(request, response, next){
     Food.find(request.params.id)
-    .then(food => response.status(200).json(food))
-    .catch(error => response.status(500).json({ error }))
+    .then(food => {
+      if (food){
+        response.json(food)
+      } else {
+        return response.sendStatus(404)
+      }
+    })
   }
 }
 
