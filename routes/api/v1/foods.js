711351ca78b4c6d053860c94b907878e69e8d7db
Anna Royer <anna.royer@colorado.edu>
2018-05-04 16:48:38 -0600

    add route to patch food

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -5,5 +5,6 @@ const FoodsController = require('../../../controllers/foodsController')
 router.get('/', FoodsController.index)
 router.get('/:id', FoodsController.show)
 router.post('/', FoodsController.create)
+router.patch('/:id', FoodsController.update)
 
 module.exports = router;
