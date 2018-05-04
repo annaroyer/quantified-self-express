bc17cbf431623f6db762d386ff59a1e0cf7531d2
Anna Royer <anna.royer@colorado.edu>
2018-05-04 00:47:48 -0600

    add show function to foodscontroller

--- controllers/foodsController.js
+++ controllers/foodsController.js
@@ -10,6 +10,13 @@ class FoodsController {
       response.status(500).json({ error })
     })
   }
+
+  static show(request, response, next){
+    let id = request.params.id
+    Food.find(id)
+    .then(food => response.status(200).json(food))
+    .catch(error => response.status(500).json({ error }))
+  }
 }
 
 module.exports = FoodsController
