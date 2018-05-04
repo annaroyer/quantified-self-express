7146791c7a6b7744386f94a8f78909c619be0cc2
Anna Royer <anna.royer@colorado.edu>
2018-05-04 00:51:17 -0600

    refactor foods controller

--- controllers/foodsController.js
+++ controllers/foodsController.js
@@ -3,17 +3,12 @@ const Food = require('../models/food')
 class FoodsController {
   static index(request, response, next){
     Food.all()
-    .then(function(foods){
-      response.status(200).json(foods)
-    })
-    .catch(function(error){
-      response.status(500).json({ error })
-    })
+    .then(foods => response.status(200).json(foods))
+    .catch(error => response.status(500).json({ error }))
   }
 
   static show(request, response, next){
-    let id = request.params.id
-    Food.find(id)
+    Food.find(request.params.id)
     .then(food => response.status(200).json(food))
     .catch(error => response.status(500).json({ error }))
   }
