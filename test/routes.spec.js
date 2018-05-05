e3b79f7f3203c04f961a5b3d16001b94e90fab94
Anna Royer <anna.royer@colorado.edu>
2018-05-05 14:29:25 -0600

    add test that a new record is created in database from post request, that a record is removed from database after delete request and that a record in the database is updated from patch request

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -7,24 +7,26 @@ const environment = process.env.NODE_ENV || 'test'
 const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
+function foodsCount(){
+  return database('foods').count('id')
+  .then(count => parseInt(count[0].count))
+}
+
 chai.use(chaiHttp)
 
-describe('API Routes', () => {
+describe('API Routes', function(){
+  this.timeout(0)
   before((done) => {
     database.migrate.latest()
     .then(() => done())
-    .catch((error) => {
-      throw error
-    })
+    .catch((error) => {throw error})
     .done()
   })
 
   beforeEach((done) => {
     database.seed.run()
     .then(() => done())
-    .catch((error) => {
-      throw error
-    })
+    .catch((error) => {throw error})
     .done()
   })
 
@@ -35,14 +37,10 @@ describe('API Routes', () => {
       .then((response) => {
         response.should.have.status(200)
         response.should.be.json
-        response.body.should.be.a('array')
-        response.body.length.should.equal(3)
-        response.body[0].should.deep.equal({id: 1, name: 'Banana', calories: 150})
-        response.body[1].should.deep.equal({id: 2, name: 'Yogurt', calories: 550})
-        response.body[2].should.deep.equal({id: 3, name: 'Apple', calories: 220})
-      })
-      .catch((error) => {
-        throw error
+        response.body.should.deep.equal([{id: 1, name: 'Banana', calories: 150},
+                                         {id: 2, name: 'Yogurt', calories: 550},
+                                         {id: 3, name: 'Apple', calories: 220}
+                                       ])
       })
     })
   })
@@ -54,11 +52,7 @@ describe('API Routes', () => {
       .then(response => {
         response.should.have.status(200)
         response.should.be.json
-        response.body.should.be.a('object')
         response.body.should.deep.equal({id: 1, name: 'Banana', calories: 150})
-      })
-      .catch((error) => {
-        throw error
       })
     })
 
@@ -68,22 +62,20 @@ describe('API Routes', () => {
       .then(response => {
         response.body.should.deep.equal({id: 2, name: 'Yogurt', calories: 550})
       })
-      .catch((error) => {
-        throw error
-      })
     })
 
-    it('returns a status code 404 if the food is not found', () => {
+    it('returns a 404 if a food with given id DNE', () => {
       return chai.request(server)
-      .get('/api/v1/foods/9')
-      .then(response => {
-        response.should.have.status(404)
-      })
+      .get('/api/v1/foods/4')
+      .then(response => response.should.have.status(404))
     })
   })
 
   describe('POST /api/v1/foods', function() {
     it('creates a new food and returns the food item if successful', () => {
+
+      foodsCount().then(count => count.should.equal(3))
+
       return chai.request(server)
       .post('/api/v1/foods')
       .send({ food: { name: 'Cheese', calories: 200 } })
@@ -95,9 +87,8 @@ describe('API Routes', () => {
         response.body.name.should.equal('Cheese')
         response.body.calories.should.equal(200)
       })
-      .catch((error) => {
-        throw error
-      })
+
+      foodsCount(),then(count => count.should.equal(4))
     })
 
     describe('returns 400 status code for unsuccessful post', () => {
@@ -105,74 +96,72 @@ describe('API Routes', () => {
         return chai.request(server)
         .post('/api/v1/foods')
         .send({ food: { calories: 5 } })
-        .then(response => {
-          response.should.have.status(400)
-        })
-        .catch(error => {
-          throw error
-        })
+        .then(response => response.should.have.status(400))
       })
 
       it('requires calories', () => {
         return chai.request(server)
         .post('/api/v1/foods')
         .send({ food: { name: "crackers" } })
-        .then(response => {
-          response.should.have.status(400)
-        })
-        .catch(error => {
-          throw error
-        })
+        .then(response => response.should.have.status(400))
       })
     })
   })
 
   describe('PATCH /api/v1/foods/:id', () => {
     it('updates an existing food', () => {
+      let food = require('../models/food')
+      let banana = {id: 1, name: "Banana", calories: 150}
+      let chocolateBanana = {id: 1,
+                             name: "Chocolate Covered Banana",
+                             calories: 500
+                            }
+
+      food.find(1).then(banana => banana.should.deep.equal(banana))
+
       return chai.request(server)
       .patch('/api/v1/foods/1')
       .send({ food: { name: "Chocolate Covered Banana", calories: 500 } })
       .then(response => {
         response.should.have.status(200)
         response.should.be.json
-        response.body.should.deep.equal({id: 1, name: "Chocolate Covered Banana", calories: 500 })
+        response.body.should.deep.equal(chocolateBanana)
       })
-      .catch((error) => {
-        throw error
-      })
+
+      food.find(1).then(banana => banana.should.deep.equal(chocolateBanana))
     })
 
-    it('returns a 404 status code if a food with the given id DNE', () => {
+    it('returns a 404 if a food with the given id DNE', () => {
       return chai.request(server)
       .patch('/api/v1/foods/9')
       .send({ food: { name: "Chocolate Covered Banana", calories: 500 } })
       .then(response => {
         response.should.have.status(404)
       })
-      .catch((error) => {
-        throw error
-      })
     })
   })
 
   describe('DELETE /api/v1/foods/:id', () => {
     it('deletes the food with the given id', () => {
+
+      foodsCount().then(count => count.should.equal(3))
+
       return chai.request(server)
       .delete('/api/v1/foods/1')
-      .then(response => {
-        response.should.have.status(204)
-      })
-      .catch((error) => {
-        throw error
-      })
+      .then(response => response.should.have.status(204))
+
+      foodsCount().then(count => count.should.equal(2))
     })
 
-    it('returns a 404 status code if the food with that id does not exist', () => {
+    it('returns a 404 if a food with the given id DNE', () => {
+
+      foodsCount().then(count => count.should.equal(3))
+
       return chai.request(server)
       .delete('/api/v1/foods/400')
-      .then(response => {
-        response.should.have.status(404)
-      })
+      .then(response => response.should.have.status(404))
+
+      foodsCount().then(count => count.should.equal(3))
     })
   })
 })
