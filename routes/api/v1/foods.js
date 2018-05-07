11b938c97977ae4f62ff724479325074a506b915
Anna Royer <anna.royer@colorado.edu>
2018-05-07 14:36:30 -0600

    add meal food routes to meal routes files

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+
 const FoodsController = require('../../../controllers/foodsController')
 
 router.get('/', FoodsController.index)
