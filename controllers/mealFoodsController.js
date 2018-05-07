70b4788d8fb9114d1aefcb8412603bd7f7e19d06
Anna Royer <anna.royer@colorado.edu>
2018-05-07 16:29:28 -0600

    add destroy method to mealFoods controller

--- controllers/mealFoodsController.js
+++ controllers/mealFoodsController.js
@@ -1,7 +1,5 @@
 const MealFood = require('../models/mealFood')
 
-const pry = require('pryjs')
-
 class MealFoodsController {
   static create(request, response, next){
     let meal_id = parseInt(request.params.meal_id)
@@ -11,6 +9,24 @@ class MealFoodsController {
       if(mealFood){
         let message = `Successfully added ${mealFood.foodName} to ${mealFood.mealName}`
         response.status(201).json({ message: message })
+      } else {
+        response.sendStatus(404)
+      }
+    })
+  }
+
+  static destroy(request, response, next) {
+    let meal_id = parseInt(request.params.meal_id)
+    let food_id = parseInt(request.params.id)
+    let message
+    MealFood.message(meal_id, food_id)
+    .then(names => {
+      if(names){
+        message = `Successfully removed ${names.foodName} from ${names.mealName}`
+        MealFood.destroy(meal_id, food_id)
+        .then(() => {
+          response.json({message: message})
+        })
       } else {
         response.sendStatus(404)
       }
