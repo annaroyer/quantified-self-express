9f2b9bc749a18cb3b06b3e5e226a6754be3ecff2
Anna Royer <anna.royer@colorado.edu>
2018-05-06 19:04:04 -0600

    seed meals and meal_foods in test and production dbs

--- db/seeds/test/mealz_foods.js
+++ db/seeds/test/mealz_foods.js
@@ -1,19 +1,19 @@
 exports.seed = function(knex, Promise){
-  return knex('mealFoods').del()
+  return knex('meal_foods').del()
     .then(function(){
-      return knex('mealFoods').insert([
-        {id: 1, mealId: 1, foodId: 1},
-        {id: 2, mealId: 1, foodId: 4},
-        {id: 3, mealId: 1, foodId: 7},
-        {id: 4, mealId: 2, foodId: 1},
-        {id: 5, mealId: 2, foodId: 5},
-        {id: 6, mealId: 2, foodId: 6},
-        {id: 7, mealId: 3, foodId: 2},
-        {id: 8, mealId: 3, foodId: 3},
-        {id: 9, mealId: 3, foodId: 7},
-        {id: 10, mealId: 4, foodId: 1},
-        {id: 11, mealId: 4, foodId: 2},
-        {id: 12, mealId: 4, foodId: 3}
+      return knex('meal_foods').insert([
+        {id: 1, meal_id: 1, food_id: 1},
+        {id: 2, meal_id: 1, food_id: 4},
+        {id: 3, meal_id: 1, food_id: 7},
+        {id: 4, meal_id: 2, food_id: 1},
+        {id: 5, meal_id: 2, food_id: 5},
+        {id: 6, meal_id: 2, food_id: 6},
+        {id: 7, meal_id: 3, food_id: 2},
+        {id: 8, meal_id: 3, food_id: 3},
+        {id: 9, meal_id: 3, food_id: 7},
+        {id: 10, meal_id: 4, food_id: 1},
+        {id: 11, meal_id: 4, food_id: 2},
+        {id: 12, meal_id: 4, food_id: 3}
       ])
     })
 }
