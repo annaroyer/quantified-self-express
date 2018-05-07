1a06ba8d210dbb4057f632c0a4b4a3ddd246efb0
Anna Royer <anna.royer@colorado.edu>
2018-05-07 14:20:44 -0600

    add meal foods controller with create function

---
+++ controllers/mealFoodsController.js
@@ -0,0 +1,17 @@
+const MealFood = require('../models/mealFood')
+
+const pry = require('pryjs')
+
+class MealFoodsController {
+  static create(request, response, next){
+    let meal_id = parseInt(request.params.meal_id)
+    let food_id = parseInt(request.params.id)
+    MealFood.create(meal_id, food_id)
+    .then(mealFood => {
+      let message = `Successfully added ${mealFood.foodName} to ${mealFood.mealName}`
+      response.status(201).json({ message: message })
+    })
+  }
+}
+
+module.exports = MealFoodsController
