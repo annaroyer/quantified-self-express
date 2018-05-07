ae8e834190eb2ff1571f4ac44e7fce1f2a66f53d
Anna Royer <anna.royer@colorado.edu>
2018-05-07 16:31:01 -0600

    add route to delete food from meal

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -7,5 +7,6 @@ const mealFoodsController = require('../../../controllers/mealFoodsController')
 router.get('/', mealsController.index)
 router.get('/:meal_id/foods', mealsController.show)
 router.post('/:meal_id/foods/:id', mealFoodsController.create)
+router.delete('/:meal_id/foods/:id', mealFoodsController.destroy)
 
 module.exports = router
