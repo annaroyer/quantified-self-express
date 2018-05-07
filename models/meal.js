67498f9b1abe878fd0baff1fa11c54381c71227f
Anna Royer <anna.royer@colorado.edu>
2018-05-06 18:57:37 -0600

    add nested promises to meal all function to get foods added to meal objects

--- models/meal.js
+++ models/meal.js
@@ -3,11 +3,23 @@ const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
 class Meal {
-
   static all(){
     return database('meals').select('id', 'name')
+    .then(meals => {
+      return Promise.all(
+        meals.map(meal => {
+          return database('foods')
+          .select('foods.id', 'foods.name', 'foods.calories')
+          .join('meal_foods', {'foods.id': 'meal_foods.food_id'})
+          .where('meal_foods.meal_id', meal.id)
+          .then(foods => {
+            meal.foods = foods
+            return meal
+          })
+        })
+      )
+     })
+    }
   }
-
-}
 
 module.exports = Meal
