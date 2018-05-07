6ca549f41a11b926d54cf9579191654cdf81ea1b
Anna Royer <anna.royer@colorado.edu>
2018-05-06 18:19:34 -0600

    remove pry from mealsController

--- controllers/mealsController.js
+++ controllers/mealsController.js
@@ -1,13 +1,10 @@
 const Meal = require('../models/meal')
-const pry = require('pryjs')
 
 class MealsController {
-
   static index(request, response, next){
     Meal.all()
     .then(meals => response.json(meals))
   }
-
 }
 
 module.exports = MealsController
