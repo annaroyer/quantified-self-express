86b1b3404596fd77392125675553ff53c5fa963c
Anna Royer <anna.royer@colorado.edu>
2018-05-07 21:48:19 -0600

    reorganize seeds and reseed

--- data/dev/foods.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-  {name: 'Banana', calories: 150},
-  {name: 'Bagel Bites - Four Cheese', calories: 650},
-  {name: 'Chicken Burrito', calories: 800},
-  {name: 'Yogurt', calories: 550},
-  {name: 'Gum', calories: 50},
-  {name: 'Cheese', calories: 400},
-  {name: 'Apple', calories: 220}
-]
--- data/dev/meals.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-  {name: 'Breakfast'},
-  {name: 'Snack'},
-  {name: 'Lunch'},
-  {name: 'Dinner'}
-]
